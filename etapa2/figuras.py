7a64d00f10af4ea9d6327b4c3dd49d8e063610af
Max Willian <maxwrm2007@gmail.com>
2026-07-01 20:49:42 -0300

    Primeira implementação da classe figuras
    Classe figuras com os métodos usados nas outras subclasses

--- etapa2/figuras.py
+++ etapa2/figuras.py
@@ -1,4 +1,19 @@
 from tkinter import *
-class Figuras():
-    def __init__(self):
-        pass
+
+class Figuras:
+    def __init__(self, event, fill="black", outline="black"):
+        self.ini_x = event.x
+        self.ini_y = event.y
+        self.fim_x = event.x
+        self.fim_y = event.y
+        self.fill = fill
+        self.outline = outline
+        
+    #Atualiza as coordenadas do ponto final da figura
+    def atualizar_coordenadas(self, event):
+        self.fim_x = event.x
+        self.fim_y = event.y
+
+    #Se os pontos iniciais e finais forem iguais, a figura está incompleta
+    def esta_incompleta(self):
+        return (self.ini_x, self.ini_y) == (self.fim_x, self.fim_y)
