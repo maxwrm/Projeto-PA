18d3a58dc702bc68b02bd797b4ae3b86b6a0a61e
Max Willian <maxwrm2007@gmail.com>
2026-07-01 21:19:46 -0300

    Classe linha

---
+++ etapa2/linha.py
@@ -0,0 +1,6 @@
+from tkinter import *
+from figuras import Figuras
+
+class Linha(Figuras):
+    def desenhar_figura(self, canvas):
+        canvas.create_line(self.ini_x, self.ini_y, self.fim_x, self.fim_y, fill=self.fill, dash=(4, 2))
