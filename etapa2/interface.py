4613e15ab9876ed80820afa8a0307ad52d428e3c
ironpeu <pedrohenriquecrmota@gmail.com>
2026-07-02 23:37:26 -0300

    Fix do metodo desfazer, botao e alterações no grid

--- etapa2/interface.py
+++ etapa2/interface.py
@@ -34,44 +34,49 @@ class Interface(Tk):
 
         # Área de desenho
         self.canvas = Canvas(self.frame, bg='white', width=1200, height=1200)
-        self.canvas.grid(column=0, row=1, columnspan=6, sticky=W, **paddings)
+        self.canvas.grid(column=0, row=2, columnspan=10, sticky=W, **paddings)
 
         # label - figuras
         label = ttk.Label(self.frame, text='Figuras:')
-        label.grid(column=0, row=0, sticky=W, **paddings)
+        label.grid(column=0, row=1, sticky=W, **paddings)
 
         # option menu - figuras
         self.tipo_figura_var = StringVar(self) # Guarda o tipo de figura selecionado no option menu (linha ou rabisco)
         option_menu = ttk.OptionMenu(self.frame, self.tipo_figura_var,'Linha', 'Linha', 'Rabisco', 'Retangulo', 'Oval', 'Circulo')
-        option_menu.grid(column=1, row=0, sticky=W, **paddings)
+        option_menu.grid(column=1, row=1, sticky=W, **paddings)
 
         # label - cores - preenchimento
         label = ttk.Label(self.frame, text='Cores de preenchimento:')
-        label.grid(column=2, row=0, sticky=W, **paddings)
+        label.grid(column=2, row=1, sticky=W, **paddings)
 
         # option menu - cores - preenchimento
         cor_preenchimento_var = StringVar(self) # Guarda a cor de preenchimento selecionada no option menu
         option_menu = ttk.OptionMenu(self.frame, self.cor_preenchimento_var, 'black', None, 'black', 'white', 'red', 'blue', 'green', 'yellow', 'orange', 'purple')
-        option_menu.grid(column=3, row=0, sticky=W, **paddings)
+        option_menu.grid(column=3, row=1, sticky=W, **paddings)
 
         # label - cores - borda
         label = ttk.Label(self.frame, text='Cores de borda:')
-        label.grid(column=4, row=0, sticky=W, **paddings)
+        label.grid(column=4, row=1, sticky=W, **paddings)
 
         # option menu - cores - borda
         cor_borda_var = StringVar(self) # Guarda a cor de borda selecionada no option menu
         option_menu = ttk.OptionMenu(self.frame, self.cor_borda_var,'black', 'black', 'white', 'red', 'blue', 'green', 'yellow', 'orange', 'purple')
-        option_menu.grid(column=5, row=0, sticky=W, **paddings)
+        option_menu.grid(column=5, row=1, sticky=W, **paddings)
 
         #Botoes
+        
+        #Botao de limpar o canvas, que remove todas as figuras do canvas e da lista de figuras
         botao_limpar = ttk.Button(self.frame, text='Limpar', command=self.limpar_canvas)
+        
         botao_limpar.grid(column=6, row=0, sticky=W, **paddings)
+        #Botao de desfazer a ultima figura desenhada, que remove a ultima figura da lista de figuras e redesenha todas as figuras no canvas
+        botao_desfazer = ttk.Button(self.frame, text='↩', command=self.desfazer)
+        botao_desfazer.grid(column=0, row=0, sticky=W, **paddings)
 
         # Eventos de mouse associados ao canvas - com seus callbacks
         self.canvas.bind('<ButtonPress-1>', self.iniciar_figura_nova)
         self.canvas.bind('<B1-Motion>', self.atualizar_figura_nova)
         self.canvas.bind('<ButtonRelease-1>', self.incluir_figura_nova)
-        self.canvas.bind('<ButtonPress-3>', self.desfazer)
 
     #Clica no botão do mouse para iniciar a criação de uma nova figura
     def iniciar_figura_nova(self, event):
@@ -106,7 +111,7 @@ class Interface(Tk):
         self.canvas.delete("all")
         self.figuras = []
 
-    def desfazer(self, event):
+    def desfazer(self):
         if self.figuras != []:
             self.figuras.remove(self.figuras[-1])
             self.desenhar_figuras()
